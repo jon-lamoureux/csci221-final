648526e7b1363b911308909d3f88ef02c1c7e55b
Jonathan_PL <jlamoureux@stetson.edu>
2021-05-03 04:13:03 -0400

    Read and write functionality working for encrypt.cpp and decrypt.cpp

--- decrypt.cpp
+++ decrypt.cpp
@@ -1,6 +1,29 @@
 #include "decrypt.h"
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int decrypt::beginDecrypt(string& inputFile, string& outputFile, char& inputType) {
+	// If we need to open a file, turn it into a string
+	string content = inputFile;
+	if (inputType == 'f') {
+		// Create variables
+		ifstream inFile(inputFile, ios::in);
+		if (inFile) {
+			// Optimal way to allocate size for string from https://insanecoding.blogspot.com/2011/11/how-to-read-in-file-in-c.html
+			inFile.seekg(0, ios::end);
+			content.reserve(inFile.tellg());
+			// Return to beginning of file for later
+			inFile.seekg(0, ios::beg);
+			// Assign contents of file to string
+			content.assign((istreambuf_iterator<char>(inFile)), istreambuf_iterator<char>());
+			inFile.close();
+		}
+	}
+	cout << content;
+	ofstream outFile(outputFile, ios::out);
+	outFile << content;
+	outFile.close();
 	return 0;
 }
--- encrypt.cpp
+++ encrypt.cpp
@@ -2,19 +2,28 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
 using namespace std;
 
 int encrypt::beginEncrypt(string& inputFile, string& outputFile, char& inputType) {
-	  ofstream myfile;
-	  myfile.open ("example.txt");
-	  myfile << "Writing this to a file.\n";
-	  myfile.close();
-	// Overall if statement to decide whether we need to load a file or not
+	// If we need to open a file, turn it into a string
+	string content = inputFile;
 	if (inputType == 'f') {
-
-	} else {
-
+		// Create variables
+		ifstream inFile(inputFile, ios::in);
+		if (inFile) {
+			// Optimal way to allocate size for string from https://insanecoding.blogspot.com/2011/11/how-to-read-in-file-in-c.html
+			inFile.seekg(0, ios::end);
+			content.reserve(inFile.tellg());
+			// Return to beginning of file for later
+			inFile.seekg(0, ios::beg);
+			// Assign contents of file to string
+			content.assign((istreambuf_iterator<char>(inFile)), istreambuf_iterator<char>());
+			inFile.close();
+		}
 	}
+	cout << content;
+	ofstream outFile(outputFile, ios::out);
+	outFile << content;
+	outFile.close();
 	return 0;
 }
