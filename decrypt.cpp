76faa5f77f8c7dab88d174b08d7d0ec30abd8ebf
Jonathan_PL <jlamoureux@stetson.edu>
2021-05-03 14:10:13 -0400

    Completed decrypt method

--- decrypt.cpp
+++ decrypt.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 int decrypt::beginDecrypt(string& inputFile, string& outputFile, char& inputType, string& keyword) {
+	string genKeyword, decryptedText;
+	// remove spaces from the string
 	// If we need to open a file, turn it into a string
 	string content = inputFile;
 	if (inputType == 'f') {
@@ -21,9 +24,37 @@ int decrypt::beginDecrypt(string& inputFile, string& outputFile, char& inputType
 			inFile.close();
 		}
 	}
-	cout << content;
+	// Since the cipher requires the key to be the same length as the string to encrypt,
+	// We need to increase the length of the key to be the same as the phrase to encrypt if it is smaller
+	for (int i = 0; genKeyword.size() != content.size(); i++) {
+		genKeyword.push_back(keyword[i]);
+		if(i == (keyword.size() - 1)) {
+			i = -1;
+		}
+	}
+	transform(genKeyword.begin(), genKeyword.end(), genKeyword.begin(), ::toupper);
+	// Decryption time
+	for(int i = 0; i < content.size(); i++) { // Decrypt each individual character
+		// according to https://www.dcode.fr/vigenere-cipher, the best way to use the cipher is to take the letter of the string's value and
+		// take the letter of the key's value and add them together, and then modulo 26 in case they are both higher than 13
+		char curr = (content[i] - genKeyword[i] + 26) % 26 + 'A';
+		// Add to encrypted string
+		decryptedText.push_back(curr);
+	}
+	cout << "The decrypted text of \"" << content << "\" is: " << decryptedText << endl;
+
 	ofstream outFile(outputFile, ios::out);
-	outFile << content;
+	outFile << decryptedText;
 	outFile.close();
+	cout << "The result of your decryption has been stored in " << outputFile << endl;
+
+	ofstream keyFile("key_" + outputFile, ios::out);
+	keyFile << keyword;
+	keyFile.close();
+
+	cout << "The key used for your decryption has been stored in key_" << outputFile << endl;
+	if (inputType == 'f' && inputFile != outputFile) {
+		cout << "Your original input file can still be found at " << inputFile << endl;
+	}
 	return 0;
 }
